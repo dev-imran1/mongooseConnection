8a659b3a056982b15bb4f8119c87ad2a816165d6
dev-imran1 <freealltime247@gmail.com>
2024-06-02 16:28:46 +0600

    app=express

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {app} from "./app.js";
 import { dbConnect } from "./db/index.js";
 
-app.listen("8000", () =>{
-    console.log('server is runing')
-})
+    app.listen("8000", () =>{
+        console.log('server is runing')
+    })
